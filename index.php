87fc28166995d8c2ea96eddfd3aca8514841d78b
Ashish <ashishranpara@gmail.com>
2018-10-04 19:25:54 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php require 'header.php';?>  <!-- /.navbar -->
+  <div class="wrapper image-wrapper h-100-less bg-image inverse-text box-shadow" data-image-src="style/Banner/Wedding.jpg">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto text-center">
+          <div class="container mx-auto">
+            <h2 class="heading">Wedding</h2>
+            <p class="lead larger">Maecenas sed diam eget risus varius blandit magna.</p>
+            <div class="space10"></div>
+            <a href="http://localhost/red9/redfinal/gallery.php" class="btn btn-full-rounded btn-white">See Gallery</a> </div>
+          <!-- /.container --> 
+        </div>
+        <!-- /column --> 
+      </div>
+      <!-- /.row --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </div>
+  <!-- /.wrapper -->
+  <div class="wrapper image-wrapper h-100-less bg-image inverse-text box-shadow" data-image-src="style/Banner/Pre-Wedding.jpg">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto text-center">
+          <div class="container mx-auto">
+            <h2 class="heading">Pre Wedding</h2>
+            <p class="lead larger">Maecenas sed diam eget risus varius blandit magna.</p>
+            <div class="space10"></div>
+            <a href="http://localhost/red9/redfinal/gallery.php" class="btn btn-full-rounded btn-white">See Gallery</a> </div>
+          <!-- /.container --> 
+        </div>
+        <!-- /column --> 
+      </div>
+      <!-- /.row --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </div>
+  <!-- /.wrapper -->
+  <div class="wrapper image-wrapper h-100-less bg-image inverse-text box-shadow" data-image-src="style/Banner/fashion-photoshoot.jpg">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto text-center">
+          <div class="container mx-auto">
+            <h2 class="heading">Fashion Photoshoot</h2>
+            <p class="lead larger">Maecenas sed diam eget risus varius blandit magna.</p>
+            <div class="space10"></div>
+            <a href="http://localhost/red9/redfinal/gallery.php" class="btn btn-full-rounded btn-white">See Gallery</a> </div>
+          <!-- /.container --> 
+        </div>
+        <!-- /column --> 
+      </div>
+      <!-- /.row --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </div>
+  <!-- /.wrapper -->
+  <div class="wrapper image-wrapper h-100-less bg-image inverse-text box-shadow" data-image-src="style/Banner/interior-photography.jpg">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto text-center">
+          <div class="container mx-auto">
+            <h2 class="heading">Interior Photography</h2>
+            <p class="lead larger">Maecenas sed diam eget risus varius blandit magna.</p>
+            <div class="space10"></div>
+            <a href="http://localhost/red9/redfinal/gallery.php" class="btn btn-full-rounded btn-white">See Gallery</a> </div>
+          <!-- /.container --> 
+        </div>
+        <!-- /column --> 
+      </div>
+      <!-- /.row --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </div>
+  <!-- /.wrapper -->
+  <div class="wrapper image-wrapper h-100-less bg-image inverse-text box-shadow" data-image-src="style/Banner/baby-bump-Photography.jpg">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto text-center">
+          <div class="container mx-auto">
+            <h2 class="heading">Baby Bump Photography</h2>
+            <p class="lead larger">Maecenas sed diam eget risus varius blandit magna.</p>
+            <div class="space10"></div>
+            <a href="http://localhost/red9/redfinal/gallery.php" class="btn btn-full-rounded btn-white">See Gallery</a> </div>
+          <!-- /.container --> 
+        </div>
+        <!-- /column --> 
+      </div>
+      <!-- /.row --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </div>
+  <!-- /.wrapper -->
+  <div class="wrapper image-wrapper h-100-less bg-image inverse-text" data-image-src="style/Banner/Kids-Photography.jpg">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-12 my-auto text-center">
+          <div class="container mx-auto">
+            <h2 class="heading">Kids Photography</h2>
+            <p class="lead larger">Maecenas sed diam eget risus varius blandit magna.</p>
+            <div class="space10"></div>
+            <a href="http://localhost/red9/redfinal/gallery.php" class="btn btn-full-rounded btn-white">See Gallery</a> </div>
+          <!-- /.container --> 
+        </div>
+        <!-- /column --> 
+      </div>
+      <!-- /.row --> 
+    </div>
+    <!-- /.container-fluid --> 
+  </div>
+  <!-- /.wrapper -->
+<?php require 'footer.php';?>
